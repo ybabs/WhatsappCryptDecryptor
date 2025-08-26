530228a27f7a1cf21732d4e4321275660f7047f6
Daniel <29642663+ybabs@users.noreply.github.com>
2025-08-26 20:49:34 +0100

    Update conan package name

--- conanfile.py
+++ conanfile.py
@@ -2,7 +2,7 @@ from conan import ConanFile
 from conan.tools.cmake import cmake_layout, CMakeToolchain, CMakeDeps
 
 class WhatsAppDecryptorConan(ConanFile):
-    name = "Whatsapp Database Decryptor"
+    name = "whatsappcryptdecryptor"
     version = "0.1.0"
 
     settings = "os", "compiler", "build_type", "arch"
